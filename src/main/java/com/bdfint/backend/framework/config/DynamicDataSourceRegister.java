818eb04a7319955d0405e2ec68c51d77cdc3c28e
fengcheng <lufengcheng@bdfint.com>
2017-10-06 16:29:55 +0800

    reformat code

--- src/main/java/com/bdfint/backend/framework/config/DynamicDataSourceRegister.java
+++ src/main/java/com/bdfint/backend/framework/config/DynamicDataSourceRegister.java
@@ -72,9 +72,9 @@ public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar,
     public DataSource buildDataSource(Map<String, Object> dsMap) {
         try {
             Object type = dsMap.get("type");
-            if (type == null)
+            if (type == null) {
                 type = DATASOURCE_TYPE_DEFAULT;// 默认DataSource
-
+            }
             Class<? extends DataSource> dataSourceType;
             dataSourceType = (Class<? extends DataSource>) Class.forName((String) type);
 
